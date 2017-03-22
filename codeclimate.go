6906065c651ff403b441220c15b9632ff2a187af
David Ko <david.ko@pvtmethod.com>
2017-03-22 18:40:56 -0400

    Add initial url parsing to GetScore method

--- codeclimate.go
+++ codeclimate.go
@@ -1,9 +1,34 @@
 package codeclimate
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+)
+
+const (
+	codeClimateApiHost = "https://api.codeclimate.com"
 )
 
 type HttpCaller interface {
 	Do(req *http.Request) (*http.Response, error)
+}
+
+type codeClimateClient struct {
+	apiKey     string
+	appId      string
+	httpClient HttpCaller
+}
+
+func New(apiKey string, appId string) codeClimateClient {
+	return codeClimateClient{
+		apiKey:     apiKey,
+		appId:      appId,
+		httpClient: http.Client{},
+	}
+}
+
+func (c codeClimateClient) GetScore() float64 {
+	u = url.Parse(codeClimateApiHost)
+	u.Path = fmt.Sprintf("/v1/repos/%s", c.appId)
 }
