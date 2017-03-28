b46c03dc37bfa8938fe860371793be5fcdd4fe7e
David Ko <david.ko@pvtmethod.com>
2017-03-28 18:33:05 -0400

    Nested structs

--- codeclimate.go
+++ codeclimate.go
@@ -15,45 +15,51 @@ type HttpCaller interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-type codeClimateClient struct {
+type client struct {
 	apiKey     string
 	appId      string
 	httpClient HttpCaller
 }
 
-func NewClient(apiKey string, appId string, caller HttpCaller) codeClimateClient {
-	return codeClimateClient{
+type Response struct {
+	Data struct {
+		Repo Repo
+	}
+}
+
+type Repo struct {
+	Score float64
+}
+
+func NewClient(apiKey string, appId string, caller HttpCaller) client {
+	return client{
 		apiKey:     apiKey,
 		appId:      appId,
 		httpClient: caller,
 	}
 }
 
-type App struct {
-	Data map[string]interface{} `json:"data"`
-}
-
-func (c codeClimateClient) GetScore() (float64, error) {
-	var app App
+func (c client) GetRepo() (Response, error) {
+	var app Response
 	u, e := url.Parse(codeClimateApiHost)
 	if e != nil {
-		return 0.0, e
+		return Response{}, e
 	}
 	u.Path = fmt.Sprintf("/v1/repos/%s", c.appId)
 	req, e := http.NewRequest(http.MethodGet, u.String(), nil)
 	if e != nil {
-		return 0.0, e
+		return Response{}, e
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("Token token=%s", c.apiKey))
 	res, e := c.httpClient.Do(req)
 	if e != nil {
-		return 0.0, e
+		return Response{}, e
 	}
 	dec := json.NewDecoder(res.Body)
 	e = dec.Decode(&app)
+	fmt.Println(app.Data)
 	if e != nil {
-		return 0.0, e
+		fmt.Println(e.Error())
 	}
-	data := app.Data["attributes"].(map[string]interface{})
-	return data["score"].(float64), nil
+	return app, nil
 }
--- codeclimate_test.go
+++ codeclimate_test.go
@@ -5,7 +5,11 @@ import (
 	"testing"
 )
 
-func TestGetScore(t *testing.T) {
-	c := NewClient("mykey", "myapp", &http.Client{})
-	c.GetScore()
+func TestGetRepo(t *testing.T) {
+	c := NewClient(
+		"ee89d8df16816e4243fd145b358793f0321d845d",
+		"53ab23a7e30ba070f000ac26",
+		&http.Client{},
+	)
+	c.GetRepo()
 }
